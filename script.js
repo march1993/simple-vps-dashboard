61fc3a3974fda648327bd68b6ff7876031530951
Wenhua Shi <march511@gmail.com>
2018-01-12 10:30:51 +0100

    update pre tag

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 'use strict';
 
 window.addEventListener('load', e => {
-        const commands = document.getElementById('commands');
+	const commands = document.getElementById('commands');
 	fetch('/cgi-bin/hostname')
 		.then(response => response.text())
 		.then(hostname => window.document.title = hostname);
-        fetch('/cgi-bin/ls')
-                .then(response => response.text())
-                .then(text => text.trim().split('\n'))
-                .then(list => list.map(command => {
-                        const
-                                tr = document.createElement('tr'),
-                                td_command = document.createElement('td'),
-                                td_result = document.createElement('td'),
-                                pre_command = document.createElement('pre'),
-                                pre_result = document.createElement('pre'),
-                                a = document.createElement('a');
+	fetch('/cgi-bin/ls')
+		.then(response => response.text())
+		.then(text => text.trim().split('\n'))
+		.then(list => list.map(command => {
+			const
+				tr = document.createElement('tr'),
+				td_command = document.createElement('td'),
+				td_result = document.createElement('td'),
+				pre_command = document.createElement('pre'),
+				pre_result = document.createElement('pre'),
+				a = document.createElement('a');
 
-                        a.target = '_BLANK';
-                        a.href = '/cgi-bin/' + command;
+			a.target = '_BLANK';
+			a.href = '/cgi-bin/' + command;
 
-                        pre_command.textContent = command;
-                        pre_result.textContent = 'loading ...';
-                        fetch('/cgi-bin/' + command)
-                                .then(response => response.text())
-                                .then(text => pre_result.textContent = text);
+			pre_command.textContent = command;
+			pre_result.textContent = 'loading ...';
+			pre_result.className = 'result';
+			fetch('/cgi-bin/' + command)
+				.then(response => response.text())
+				.then(text => pre_result.textContent = text);
 
-                        a.appendChild(pre_command);
-                        td_command.appendChild(a);
-                        td_result.appendChild(pre_result);
-                        tr.appendChild(td_command);
-                        tr.appendChild(td_result);
-                        commands.appendChild(tr);
+			a.appendChild(pre_command);
+			td_command.appendChild(a);
+			td_result.appendChild(pre_result);
+			tr.appendChild(td_command);
+			tr.appendChild(td_result);
+			commands.appendChild(tr);
 
-                        return command;
-        }));
+			return command;
+	}));
 });
