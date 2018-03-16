2a66f8673245df8abf6c24b53de85444a8b10c6e
Wenhua Shi <march511@gmail.com>
2018-03-16 13:20:54 +0100

    update

--- script.js
+++ script.js
@@ -35,5 +35,30 @@ window.addEventListener('load', e => {
 			commands.appendChild(tr);
 
 			return command;
-	}));
+		}));
+
+
+	/* Ping */
+	const d_ping = document.getElementById('ping');
+	const delays = [];
+	const average_window = 5;
+	const measure_one = () => {
+		const tick = +new Date;
+		fetch('/cgi-bin/false').then(() => {
+			const tock = +new Date;
+			delays.unshift(tock - tick);
+			delays.splice(average_window);
+
+			const sum = delays.reduce((a, b) => a + b, 0);
+			const avg = sum / delays.length;
+			d_ping.textContent = avg + 'ms';
+			measure_one();
+		}).catch(e => {
+			delays.splice(0);
+			d_ping.textContent = 'Network Error';
+			measure_one();
+		});
+	};
+	measure_one();
+
 });
